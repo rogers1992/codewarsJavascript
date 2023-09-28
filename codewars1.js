5a2c4c34c38fe52885f3465a499a46becaa3f52f
rogers1992 <rogerulaque@gmail.com>
2023-09-28 19:19:46 -0400

    codeWars resoled

--- codewars1.js
+++ codewars1.js
@@ -3,9 +3,46 @@ ends with the 2nd argument (also a string).
 Examples:
 
 solution('abc', 'bc') // returns true
-solution('abc', 'd') // returns false */
+solution('abc', 'd') // returns false
+solution('abcdca', 'dca') // returns false */
+
+console.log("Hola mundo")
 
 function compareString(string1, string2)
 {
-    return "result"
+    let result = false
+    let actualPosition = 0
+    let index = 0
+    let stringExtracted = string1.slice(index, string2.length)
+    console.log(stringExtracted)
+    if(stringExtracted.localeCompare(string2) == 0){
+        result = true 
+        console.log(result)
+        return result
+        
+    }
+    else{
+        if (index == string1.length - string2.length){
+            console.log(result)
+            return result
+        }
+        else{
+            index =+ 1
+            let newString = string1.slice(index, string1.length) 
+            compareString(newString, string2)
+        }
+        
+    }
+    
+    
 }
+compareString("roger", "op")
+
+
+function split(str, index){
+    let result = [str.slice(0,index), str.slice(index)]
+    return result
+}
+
+console.log(split("abcd", 2))
+//console.log(split(2))
